af77a1219e82e02fa282d318ebc5547ea74dd521
Bae Ha-eun <dncl46@gmail.com>
2023-06-13 02:05:10 +0900

    이미지경로 수정

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,6 @@ export default {
     email: 'dncl46@gmail.com',
     github: 'https://github.com/unaaa12',
     phone: '+82-10-1234-5678',
-    image: 'https://heropy.blog/css/images/logo.png'
+    image: 'https://github.com/unaaa12/vue3-movie-app/blob/main/src/assets/rabbit.jpg?raw=true'
   })
 }
